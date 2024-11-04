87af4e44d3ec8d8c05182ca9dfb75e20f597f4a6
Bharath Mohan <bharath.mohan.pro@gmail.com>
2024-11-04 15:18:33 -0800

    simplied nav

--- portfolio/components/nav.py
+++ portfolio/components/nav.py
@@ -3,56 +3,28 @@ from portfolio.state import State
 
 
 def nav_section() -> rx.Component:
+    nav_items = [
+        ("Home", "", "/"),
+        ("Projects", "projects", "/projects"),
+        ("Work", "work", "/work"),
+    ]
+
     return rx.center(
         rx.hstack(
-            rx.cond(
-                State.get_page == "",
+            *[
                 rx.link(
                     rx.box(
-                        rx.text("Home", class_name="text-2xl"),
-                        class_name="border-b-2",
+                        rx.cond(
+                            State.get_page == route,
+                            rx.text(text, class_name="text-2xl border-b-2"),
+                            rx.text(text, class_name="text-2xl"),
+                        ),
                     ),
                     class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/",
-                ),
-                rx.link(
-                    rx.text("Home", class_name="text-2xl"),
-                    class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/",
-                ),
-            ),
-            rx.cond(
-                State.get_page == "projects",
-                rx.link(
-                    rx.box(
-                        rx.text("Projects", class_name="text-2xl"),
-                        class_name="border-b-2",
-                    ),
-                    class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/projects",
-                ),
-                rx.link(
-                    rx.text("Projects", class_name="text-2xl"),
-                    class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/projects",
-                ),
-            ),
-            rx.cond(
-                State.get_page == "work",
-                rx.link(
-                    rx.box(
-                        rx.text("Work", class_name="text-2xl"),
-                        class_name="border-b-2",
-                    ),
-                    class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/work",
-                ),
-                rx.link(
-                    rx.text("Work", class_name="text-2xl"),
-                    class_name="text-inherit cursor-pointer no-underline hover:text-inherit",
-                    href="/work",
-                ),
-            ),
+                    href=href,
+                )
+                for text, route, href in nav_items
+            ],
             class_name="flex justify-around items-center mb-20 mt-10 gap-12 md:gap-32",
         ),
     )
